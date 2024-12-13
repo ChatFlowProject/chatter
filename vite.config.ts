a7fa47f35df04954b0474ea16aebd82887e0a9f7
sangjin149 <sangjin149@gmail.com>
2024-12-13 18:46:12 +0900

    config: alias 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    resolve: {
+        alias: [
+            { find: "@/", replacement: path.resolve(__dirname, "./src/") },
+            { find: "@api/", replacement: path.resolve(__dirname, "./src/api") },
+            { find: "@assets/", replacement: path.resolve(__dirname, "./src/assets") },
+            { find: "@components/", replacement: path.resolve(__dirname, "./src/components") },
+            { find: "@hooks/", replacement: path.resolve(__dirname, "./src/hooks") },
+            { find: "@store/", replacement: path.resolve(__dirname, "./src/store") },
+            { find: "@types/", replacement: path.resolve(__dirname, "./src/types") },
+            { find: "@utils/", replacement: path.resolve(__dirname, "./src/utils") },
+        ],
+    },
+});
