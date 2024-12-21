a952f27200cd2bf9f8e5fc53c19bf1253ccc8fcd
sangjin149 <sangjin149@gmail.com>
2024-12-21 17:31:38 +0900

    fix(css): LoginLabel tailwind 기입 순서 수정

--- src/pages/SignupPage/LoginLabel.tsx
+++ src/pages/SignupPage/LoginLabel.tsx
@@ -5,7 +5,7 @@ interface LoginLabelProps {
 
 export default function LoginLabel({ title, children }: LoginLabelProps) {
   return (
-    <label className='flex flex-col mt-5 items-stretch gap-2'>
+    <label className='mt-5 flex flex-col items-stretch gap-2'>
       <div className='text-[#B5BAC1] text-xs text-left'>{title}</div>
       {children}
     </label>
