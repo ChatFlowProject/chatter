af6d4556f0020f80b03cf1568f87e02c55829f66
sangjin149 <sangjin149@gmail.com>
2024-12-21 18:11:20 +0900

    feature(useForm): 제출 함수 구현

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -10,16 +10,10 @@ interface formInfo {
   validation: validation
 }
 
-interface useFormProps {
-  formValueInfos: formInfos
-  //   onSubmit(): void
-}
-
 // 요구사항:
 // 1. 값 제어
 // - 전체 값 초기화 함수 반환
 // - 필수여부
-// 2. 제출 클릭시 콜백 실행
 
 function initializeInputInfos(infos: formInfos) {
   const initialValues: Record<valueName, inputValue> = {}
@@ -34,13 +28,21 @@ function initializeInputInfos(infos: formInfos) {
   return { initialValues, validations }
 }
 
-export default function useForm({ formValueInfos }: useFormProps) {
+export default function useForm(
+  formValueInfos: formInfos,
+  submitCallback: () => void,
+): {
+  values: Record<valueName, inputValue>
+  errors: Record<valueName, string>
+  onChange: (newValue: inputValue, valueName: valueName) => void
+  onSubmit: () => void
+} {
   const { initialValues, validations } = initializeInputInfos(formValueInfos)
   const [values, setValues] =
     useState<Record<valueName, inputValue>>(initialValues)
   const [errors, setErrors] = useState<Record<valueName, string>>({})
 
-  function onChange(newValue: inputValue, valueName: valueName) {
+  function onChange(newValue: inputValue, valueName: valueName): void {
     const errorMessage = validations[valueName](newValue)
     setErrors((prevErrors) => ({ ...prevErrors, [valueName]: errorMessage }))
 
@@ -52,11 +54,19 @@ export default function useForm({ formValueInfos }: useFormProps) {
     }
   }
 
+  function onSubmit(): void {
+    for (const valueName in errors) {
+      if (errors[valueName] !== '')
+        return console.error('아직 오류가 남아있습니다')
+    }
+    submitCallback()
+  }
+
   return {
     values,
     errors,
     onChange,
+    onSubmit,
     // isEssential,
-    // errorMessage,
   }
 }
