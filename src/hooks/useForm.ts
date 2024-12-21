5a6a756fa774c0ccdfbd94fc0bd167e8aad0d08f
sangjin149 <sangjin149@gmail.com>
2024-12-21 18:25:46 +0900

    feature(useForm): 필수항목 관련 기능 추가 - 필수 항목 미작성시 submitCallback 미호출 - useForm의 반환객체에 필수항목 여부 포함

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -6,7 +6,7 @@ type formInfos = Record<valueName, formInfo>
 type validation = (newValue: inputValue) => string
 interface formInfo {
   initialValue: inputValue
-  //   isEssential: boolean
+  isEssential: boolean
   validation: validation
 }
 
@@ -18,14 +18,16 @@ interface formInfo {
 function initializeInputInfos(infos: formInfos) {
   const initialValues: Record<valueName, inputValue> = {}
   const validations: Record<valueName, validation> = {}
+  const isEssentials: Record<valueName, boolean> = {}
 
   for (const valueName in infos) {
-    const { initialValue, validation } = infos[valueName]
+    const { initialValue, isEssential, validation } = infos[valueName]
     initialValues[valueName] = initialValue
     validations[valueName] = validation
+    isEssentials[valueName] = isEssential
   }
 
-  return { initialValues, validations }
+  return { initialValues, validations, isEssentials }
 }
 
 export default function useForm(
@@ -34,10 +36,15 @@ export default function useForm(
 ): {
   values: Record<valueName, inputValue>
   errors: Record<valueName, string>
+  isEssential: Record<valueName, boolean>
   onChange: (newValue: inputValue, valueName: valueName) => void
   onSubmit: () => void
 } {
-  const { initialValues, validations } = initializeInputInfos(formValueInfos)
+  const {
+    initialValues,
+    validations,
+    isEssentials: isEssential,
+  } = initializeInputInfos(formValueInfos)
   const [values, setValues] =
     useState<Record<valueName, inputValue>>(initialValues)
   const [errors, setErrors] = useState<Record<valueName, string>>({})
@@ -55,7 +62,9 @@ export default function useForm(
   }
 
   function onSubmit(): void {
-    for (const valueName in errors) {
+    for (const valueName in values) {
+      if (isEssential[valueName] && values[valueName] === '')
+        return console.error('필수 항목을 작성해주세요')
       if (errors[valueName] !== '')
         return console.error('아직 오류가 남아있습니다')
     }
@@ -65,6 +74,7 @@ export default function useForm(
   return {
     values,
     errors,
+    isEssential,
     onChange,
     onSubmit,
     // isEssential,
