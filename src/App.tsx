60abd866beffde06716f734551e73e2b505b12e9
SJ-1220 <kindjin12@naver.com>
2024-12-22 12:25:35 +0900

    feat(tailwind): tailwind.config 폰트, 글자 크기, 색상 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import "./App.css";
+import './App.css'
 
 function App() {
-    return (
-        <>
-            <h1 className="text-3xl underline">Hello world!</h1>
-        </>
-    );
+  return (
+    <>
+      <h1 className='text-3xl underline'>test world!</h1>
+      <p className='font-regular'>regular</p>
+      <p className='font-kr'>코리안</p>
+      <div className='bg-sidebar text-white p-4'>
+        <p className='font-bold'>bold</p>
+      </div>
+      <div className='bg-chat text-white p-4'>
+        <p className='font-arial'>arial</p>
+      </div>
+      <div className='bg-wrapper text-white p-4'>
+        <p className='font-regular'>regular</p>
+      </div>
+      <p className='font-sans'>arial</p>
+      <p className='font-medium'>arial</p>
+    </>
+  )
 }
 
-export default App;
+export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,23 @@ export default {
     './src/App.tsx',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'wrapper': '#313338',
+        'sidebar': '#2E3036',
+        'chat': '#37393F',
+      },
+      fontSize: {},
+      fontFamily: {
+        regular: ['gg sans Regular', 'sans-serif'],
+        bold: ['gg sans Bold', 'sans-serif'],
+        medium: ['gg sans Medium', 'sans-serif'],
+        semibold: ['gg sans Semibold', 'sans-serif'],
+        kr: ['NotoSansKR-VariableFont_wght', 'sans-serif'],
+        arial: ['Arial', 'sans-serif'],
+        sans: ['sans-serif', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
