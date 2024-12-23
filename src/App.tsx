d800a747517ccee1bfff86eec8a958a708187b70
SJ-1220 <kindjin12@naver.com>
2024-12-23 15:20:44 +0900

    chore(App.tsx): 기본으로 작성

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,6 @@ function App() {
   return (
     <>
       <h1 className='text-3xl underline'>test world!</h1>
-      <p className='font-regular'>regular</p>
-      <p className='font-kr'>코리안</p>
-      <div className='bg-sidebar text-white p-4'>
-        <p className='font-bold'>bold</p>
-      </div>
-      <div className='bg-chat text-white p-4'>
-        <p className='font-arial'>arial</p>
-      </div>
-      <div className='bg-wrapper text-white p-4'>
-        <p className='font-regular'>regular</p>
-      </div>
-      <p className='font-sans'>arial</p>
-      <p className='font-medium'>arial</p>
     </>
   )
 }
