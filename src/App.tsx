24c793156e0e613202fd1e89446bd713362a159e
sangjin149 <sangjin149@gmail.com>
2025-01-01 19:02:23 +0900

    feature: 웹 진입시 login페이지로 진행하는 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { BrowserRouter, Routes, Route } from 'react-router'
 import Login from './pages/Login/Login'
 import SignupPage from './pages/SignupPage/SignupPage'
 import LoginBackground from '@pages/LoginBackground'
+import Welcome from '@pages/Welcome'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={''} element={<LoginBackground />}>
+        <Route path={''} element={<Welcome />} />
+        <Route element={<LoginBackground />}>
           <Route path={'login'} element={<Login />} />
           <Route path={'signup'} element={<SignupPage />} />
         </Route>
