8c0822587fe1352a306ac66383398937deb1e19b
sangjin149 <sangjin149@gmail.com>
2025-01-05 22:02:36 +0900

    feature(Login, LoginForm): Form과 관련없는 레이아웃 분리

--- src/pages/Login/components/LoginForm.tsx
+++ src/pages/Login/components/LoginForm.tsx
@@ -2,7 +2,7 @@ import { FieldInfos, FieldValue } from 'src/types/LoginTypes'
 import { isEmptyString } from '@utils/stringValidations.ts'
 import useForm from '@hooks/useForm'
 import Button from '@components/Button'
-import { Link } from 'react-router-dom'
+
 import LoginTextInput from '@components/LoginTextInput'
 
 const fieldInfos: FieldInfos = {
@@ -36,14 +36,6 @@ function LoginForm() {
 
   return (
     <form className='w-full max-w-lg'>
-      <div className='text-center mb-6'>
-        <h1 className='text-[22.5px] font-semibold text-[#f2f2f4] mb-2'>
-          돌아오신 것을 환영해요!
-        </h1>
-        <p className='text-[15px] text-[#b5b9c0]'>
-          다시 만나다니 너무 반가워요!
-        </p>
-      </div>
       <LoginTextInput
         name='email'
         type='email'
@@ -68,18 +60,10 @@ function LoginForm() {
       >
         비밀번호를 잊으셨나요?
       </a>
-
       <Button
         style='w-full bg-[#5865f2] hover:bg-blue-700 rounded text-white py-2'
         children='로그인'
       />
-
-      <p className='text-[13.6px] text-left text-[#949aa3] mt-2'>
-        계정이 필요하신가요?{' '}
-        <Link to='/signup' className='text-[#00a9fb] hover:underline'>
-          가입하기
-        </Link>
-      </p>
     </form>
   )
 }
