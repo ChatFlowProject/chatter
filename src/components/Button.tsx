4cddf22be82ebcb316a5b0f9fef73ea1c8c7b45d
SJ-1220 <kindjin12@naver.com>
2025-01-06 17:22:54 +0900

    feat(button): onClick 속성 추가

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,7 @@
 interface ButtonProps {
   style: string
   children: React.ReactNode
+  onClick?: () => void
 }
 export default function Button({ style, children }: ButtonProps) {
   return <button className={`${style}`}>{children}</button>
