c966489a5e59de3ef000fcb35cac0bd0d76c1a7e
SJ-1220 <kindjin12@naver.com>
2025-01-06 19:00:50 +0900

    feat(chat): 채팅 페이지 구현

--- src/pages/ChatPage/ChatPage.tsx
+++ src/pages/ChatPage/ChatPage.tsx
@@ -1,4 +1,63 @@
-import Text from '../../components/Text'
+import ChatServer from './ChatServer'
+import ChatChannel from './ChatChannel'
+import ChatMessage from './ChatMessage'
+import Text from '@components/Text.tsx'
+import { useState } from 'react'
+import ChatInput from './ChatInput'
+
 export default function ChatPage() {
-  return <Text>Chatpage</Text>
+  const [activeServer, setActiveServer] = useState<number | null>(null)
+  const [activeChannel, setActiveChannel] = useState<number | null>(null)
+  const [messages, setMessages] = useState<string[]>([])
+
+  const servers = [
+    { id: 1, title: '서버1' },
+    { id: 2, title: 'server2' },
+    { id: 3, title: '추가' },
+  ]
+  const channels = [
+    { id: 1, title: '일반1' },
+    { id: 2, title: '일반2' },
+  ]
+
+  const handleSendMessage = (message: string) => {
+    setMessages([...messages, message])
+  }
+
+  return (
+    <div className='flex w-screen h-screen'>
+      <div className='wrapper'>
+        {servers.map((server) => (
+          <ChatServer
+            key={server.id}
+            title={server.title}
+            isActive={activeServer === server.id}
+            onClick={() => setActiveServer(server.id)}
+          />
+        ))}
+      </div>
+      <div className='sidebar'>
+        <Text className='smoff'>채팅채널</Text>
+        {channels.map((channel) => (
+          <ChatChannel
+            key={channel.id}
+            title={channel.title}
+            isActive={activeChannel === channel.id}
+            onClick={() => setActiveChannel(channel.id)}
+          />
+        ))}
+        <div className='panel'>user</div>
+      </div>
+      <div className='chat'>
+        <div>
+          {messages.map((message, index) => (
+            <ChatMessage key={index} message={message} />
+          ))}
+        </div>
+        <div>
+          <ChatInput onSendMessage={handleSendMessage} />
+        </div>
+      </div>
+    </div>
+  )
 }
