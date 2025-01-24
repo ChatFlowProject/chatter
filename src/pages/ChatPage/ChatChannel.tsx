9357239ccd03003cfeb1249c870de003b2bbdfa7
SJ-1220 <kindjin12@naver.com>
2025-01-24 16:07:59 +0900

    chore(chat): 디자인 작은 변경

--- src/pages/ChatPage/ChatChannel.tsx
+++ src/pages/ChatPage/ChatChannel.tsx
@@ -10,14 +10,12 @@ export default function ChatChannel({
   onClick,
 }: ChatChannelProps) {
   return (
-    <div>
-      <button
-        type='button'
-        className={`w-full p-[5px] ${isActive ? 'bg-primary' : ''} font-regular text-white  text-lg  text-left`}
-        onClick={onClick}
-      >
-        <div className='lgon'>{title}</div>
-      </button>
-    </div>
+    <button
+      type='button'
+      className={`w-full p-[5px] ${isActive ? 'bg-primary' : ''} font-regular text-white  text-lg  text-left`}
+      onClick={onClick}
+    >
+      <div className='lgon'>{title}</div>
+    </button>
   )
 }
