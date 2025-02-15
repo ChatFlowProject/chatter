1c83aab119275806cc499d132aca2823a7b2870e
dbswl701 <dbswl701@ajou.ac.kr>
2025-02-15 23:36:37 +0900

    feat: Context Menu 기본 틀 구현

---
+++ src/components/shared/ContextMenu.tsx
@@ -0,0 +1,47 @@
+// import RightArrow from '../../assets/right-arrow.svg'
+import RightArrow from '../../assets/right-arrow.svg'
+function Item({
+  name,
+  isDropdown,
+  isWarning,
+}: {
+  name: string
+  isDropdown?: boolean
+  isWarning?: boolean
+}) {
+  console.log('RightArrow', RightArrow)
+  return (
+    <div
+      className={`hover:bg-primary ${isWarning && 'hover:bg-red'} rounded-[3px] bg-[#202225] flex w-[117px] justify-between items-center px-2 py-1`}
+    >
+      <div className='font-["Whitney"] font-medium text-[#DCDDDE] text-[11.5px]'>
+        {name}
+      </div>
+      {isDropdown && (
+        <div>
+          <img src={RightArrow} alt='dropdown btn' />
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default function ContextMenu() {
+  return (
+    <div className='bg-[#202225] inline-flex flex-col p-1'>
+      <Item name='프로필' />
+      <Item name='멘션' />
+      <Item name='메시지' />
+      <Item name='통화' />
+      <Item name='메모 추가하기' />
+      <div className='h-[1px] my-1 bg-[#2F2F34] ' />
+      <Item name='앱' isDropdown />
+      <Item name='서버에 초대하기' isDropdown />
+      <Item name='친구 초대하기' />
+      <Item name='거절' />
+      <Item name='차단하기' isWarning />
+      <div className='h-[1px]  bg-[#2F2F34] ' />
+      <Item name='관리자 뷰' />
+    </div>
+  )
+}
