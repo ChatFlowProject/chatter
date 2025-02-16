0f2a1fe75e09fadf4b8f1266ec6af5c7fb5bcf61
dbswl701 <dbswl701@ajou.ac.kr>
2025-02-16 16:44:52 +0900

    feat: 여러 sub menu에 대한 호버 이동 구현

--- src/components/shared/ContextMenu.tsx
+++ src/components/shared/ContextMenu.tsx
@@ -1,4 +1,4 @@
-// import RightArrow from '../../assets/right-arrow.svg'
+import { Dispatch, SetStateAction, useRef, useState } from 'react'
 import RightArrow from '../../assets/right-arrow.svg'
 function Item({
   name,
@@ -11,10 +11,9 @@ function Item({
   isWarning?: boolean
   children?: React.ReactNode
 }) {
-  console.log('RightArrow', RightArrow)
   return (
     <div
-      className={`group relative hover:bg-primary ${isWarning && 'hover:bg-red'} rounded-[3px] bg-[#202225] flex w-[117px] justify-between items-center px-2 py-1`}
+      className={` relative hover:bg-primary ${isWarning && 'hover:bg-red'} rounded-[3px] bg-[#202225] flex w-[117px] justify-between items-center px-2 py-1`}
     >
       <div className='font-["Whitney"] font-medium text-[#DCDDDE] text-[11.5px]'>
         {name}
@@ -29,17 +28,59 @@ function Item({
   )
 }
 
-function SubMenu() {
+function SubMenu({
+  name,
+  list,
+  openSubMenu,
+  setOpenSubMenu,
+}: {
+  name: string
+  list: string[]
+  openSubMenu: string | null
+  setOpenSubMenu: Dispatch<SetStateAction<string | null>>
+}) {
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null)
+
+  const handleMouseEnter = () => {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current)
+    }
+    setOpenSubMenu(name)
+  }
+
+  const handleMouseLeave = () => {
+    timeoutRef.current = setTimeout(() => {
+      setOpenSubMenu((prevOpenSubMenu: string | null) => {
+        return prevOpenSubMenu === name ? null : prevOpenSubMenu
+      })
+    }, 300)
+  }
+
   return (
-    <div className='group-hover:visible invisible absolute left-[125px] bg-[#202225] inline-flex flex-col p-1 rounded-[5px]'>
-      <Item name='서버 A' />
-      <Item name='서버 B' />
-      <Item name='서버 C' />
-      <Item name='서버 D' />
+    <div
+      className='relative'
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <Item name={name} isDropdown />
+
+      {openSubMenu === name && (
+        <div
+          className='absolute left-[125px] top-0 bg-[#202225] flex flex-col p-1 rounded-[5px] shadow-md'
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+          {list.map((item, index) => (
+            <Item key={index} name={item} />
+          ))}
+        </div>
+      )}
     </div>
   )
 }
 export default function ContextMenu() {
+  const [openSubMenu, setOpenSubMenu] = useState<string | null>(null)
+
   return (
     <div className='bg-[#202225] inline-flex flex-col p-1 rounded-[5px]'>
       <Item name='프로필' />
@@ -48,12 +89,18 @@ export default function ContextMenu() {
       <Item name='통화' />
       <Item name='메모 추가하기' />
       <div className='h-[1px] my-1 bg-[#2F2F34] ' />
-      <Item name='앱' isDropdown>
-        <SubMenu />
-      </Item>
-      <Item name='서버에 초대하기' isDropdown>
-        <SubMenu />
-      </Item>
+      <SubMenu
+        name='앱'
+        list={['1']}
+        openSubMenu={openSubMenu}
+        setOpenSubMenu={setOpenSubMenu}
+      />
+      <SubMenu
+        name='서버에 초대하기'
+        list={['2']}
+        openSubMenu={openSubMenu}
+        setOpenSubMenu={setOpenSubMenu}
+      />
       <Item name='친구 초대하기' />
       <Item name='거절' />
       <Item name='차단하기' isWarning />
