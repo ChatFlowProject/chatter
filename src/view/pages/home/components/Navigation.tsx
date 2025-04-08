f41af8c2708a988913532410da8614041d2c2f1a
dbswl701 <dbswl701@ajou.ac.kr>
2025-04-08 17:12:37 +0900

    style: DM navigation bar 스타일 변경

--- src/view/pages/home/components/Navigation.tsx
+++ src/view/pages/home/components/Navigation.tsx
@@ -21,35 +21,31 @@ const Navigation = () => {
             <div className='w-[22px] h-[22px]'>
               <Icon path='navfriends' />
             </div>
-            <p className='text-lg font-bold'>친구</p>
+            <p className='text-lg font-bold text-neutral-400'>친구</p>
           </div>
           <div className='w-[1px] h-[24px] bg-[#42454A]' />
-          <div className='flex justify-center items-center gap-[11px]'>
+          <div className='flex justify-center items-center'>
             <NavigationButton
               isActive={activeButton === 'Online'}
               label='온라인'
               onClick={() => handleButtonClick('Online')}
-              paddingX={7}
             />
             <NavigationButton
               isActive={activeButton === 'All'}
               label='모두'
               onClick={() => handleButtonClick('All')}
-              paddingX={15}
             />
             <NavigationButton
               isActive={activeButton === 'Pending'}
               label='대기 중'
               onClick={() => handleButtonClick('Pending')}
-              paddingX={9}
             />
-            <button
-              className='flex py-[3px] px-[7px] justify-center items-center gap-[10px] rounded-[3px] bg-[#3BA55D]'
+            <NavigationButton
+              isActive={activeButton === 'Pending'}
+              isAddFriend={true}
+              label='친구 추가하기'
               onClick={handleAddFriendClick}
-              type='button'
-            >
-              친구 추가하기
-            </button>
+            />
           </div>
         </div>
       </div>
--- src/view/pages/home/components/NavigationButton.tsx
+++ src/view/pages/home/components/NavigationButton.tsx
@@ -1,30 +1,32 @@
 interface NavigationButtonProps {
-  label: string
-  paddingX?: number // 🔹 px 값을 유동적으로 받음 (기본값 설정 가능)
-  isActive?: boolean
-  onClick?: () => void
-  notification?: number
+  label: string;
+  isActive?: boolean;
+  onClick?: () => void;
+  notification?: number;
+  isAddFriend?: boolean;
 }
 
 const NavigationButton = ({
   label,
-  paddingX = 10,
   isActive,
   onClick,
   notification,
+  isAddFriend,
 }: NavigationButtonProps) => {
   return (
     <div
-      className={`flex py-[3px] px-[${paddingX}px] justify-center items-center gap-[10px] rounded-[3px] text-lg text-white cursor-pointer
-        ${isActive ? 'bg-[#3E4249]' : 'hover:bg-[#3E4249]'}`}
+      className={`flex py-[4px] px-[12px] justify-center items-center gap-[10px] rounded-[3px] text-lg text-white cursor-pointer font-bold mx-2
+        ${isAddFriend ? 'bg-primary text-white hover:bg-primary-hover' : isActive ? 'bg-[#3E4249]' : 'hover:bg-[#3E4249] text-neutral-400'}`}
       onClick={onClick}
     >
       {label}
-      {notification ? <div className='flex h-[16px] py-0 px-[5px] flex-col items-center gap-[10px] rounded-lg bg-[#ED4245]'>
+      {notification ? (
+        <div className='flex h-[16px] py-0 px-[5px] flex-col items-center gap-[10px] rounded-lg bg-[#ED4245]'>
           {notification}
-        </div> : null}
+        </div>
+      ) : null}
     </div>
-  )
-}
+  );
+};
 
-export default NavigationButton
+export default NavigationButton;
