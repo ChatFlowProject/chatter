9b0b67cdd7ae70c30dd674737b13ddc4ef39e2c9
dbswl701 <dbswl701@ajou.ac.kr>
2025-04-08 10:36:09 +0900

    feat: Topbar 추가

--- src/components/layout/AfterLoginLayout.tsx
+++ src/components/layout/AfterLoginLayout.tsx
@@ -1,8 +1,10 @@
+import Topbar from '@pages/home/components/Topbar';
 import { Outlet } from 'react-router-dom';
 
 export default function AfterLoginLayout() {
   return (
     <div className=''>
+      <Topbar />
       <Outlet />
     </div>
   );
