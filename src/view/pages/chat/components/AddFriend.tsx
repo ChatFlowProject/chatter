3bf750dea851565ed46b6919e593a55334ac1a14
dbswl701 <dbswl701@ajou.ac.kr>
2025-04-15 14:45:52 +0900

    style: 친구 추가의 Input outline 조절

--- src/view/pages/chat/components/AddFriend.tsx
+++ src/view/pages/chat/components/AddFriend.tsx
@@ -16,13 +16,13 @@ const AddFriend = () => {
     >
       <h2 className='text-[20px] font-bold mb-2'>친구 추가하기</h2>
       <h3>Discord 사용자명을 사용하여 친구를 추가할 수 있어요.</h3>
-      <div className='bg-[#2F3136] flex mt-4 px-3 h-[54px] items-center'>
+      <div className='bg-[#2F3136] flex mt-4 px-3 h-[54px] items-center rounded-[8px] focus-within:outline focus-within:outline-2 focus-within:outline-primary'>
         <input
           placeholder='Discord 사용자명을 사용하여 친구를 추가할 수 있어요.'
           onChange={(e) => setValue(e.target.value)}
-          className='flex-1 bg-inherit'
+          className='flex-1 bg-inherit outline-none'
         />
-        <button className='bg-primary px-4 py-[2px] h-[32px] rounded-[8px]'>
+        <button className='bg-primary px-4 py-[2px] h-[32px] rounded-[8px] font-bold'>
           친구 요청 보내기
         </button>
       </div>
