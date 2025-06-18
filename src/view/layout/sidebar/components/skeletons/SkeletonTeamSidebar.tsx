cd24b46f41d74a4261c035feefedb7e3a9b3cd99
dbswl701 <dbswl701@ajou.ac.kr>
2025-06-18 17:40:26 +0900

    feat: 팀 사이드바에 스켈레톤 추가

---
+++ src/view/layout/sidebar/components/skeletons/SkeletonTeamSidebar.tsx
@@ -0,0 +1,14 @@
+const SkeletonTeamSidebar = () => {
+  return (
+    <div className='wrapper flex pt-5'>
+      <div className='rounded-[13px] w-[48px] h-[48px] mt-[6px] border-[#3E4147] bg-[#3E4147] animate-pulse'></div>
+      <div className='rounded-[13px] w-[48px] h-[48px] mt-[6px] border-[#3E4147] bg-[#3E4147] animate-pulse'></div>
+      <div className='rounded-[13px] w-[48px] h-[48px] mt-[6px] border-[#3E4147] bg-[#3E4147] animate-pulse'></div>
+      <div className='rounded-[13px] w-[48px] h-[48px] mt-[6px] border-[#3E4147] bg-[#3E4147] animate-pulse'></div>
+      <div className='rounded-[13px] w-[48px] h-[48px] mt-[6px] border-[#3E4147] bg-[#3E4147] animate-pulse'></div>
+      <div className='rounded-[13px] w-[48px] h-[48px] mt-[6px] border-[#3E4147] bg-[#3E4147] animate-pulse'></div>
+    </div>
+  );
+};
+
+export default SkeletonTeamSidebar;
